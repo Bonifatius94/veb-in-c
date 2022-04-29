139f880dfc38e0be428abbf19be038fd30164b12
Marco Tröster <marco@troester-gmbh.de>
2022-04-30 00:46:05 +0200

    fix issue with stack overflow, use malloc instead

--- test/sorting_benchmark.c
+++ test/sorting_benchmark.c
@@ -52,9 +52,12 @@ void linear_shuffle(uint64_t keys[], size_t num_keys)
 
 int main(int argc, char** argv)
 {
-    size_t i, num_keys = 500000;
-    uint64_t keys[500000], sorted_keys[500000];
-    clock_t start, end; double elapsed;
+    size_t i, t, num_keys = 500000, test_runs = 100;
+    uint64_t *keys, *sorted_keys;
+    clock_t start, end; double elapsed = 0;
+
+    keys = malloc(sizeof(uint64_t) * num_keys);
+    sorted_keys = malloc(sizeof(uint64_t) * num_keys);
 
     /* generate random distributed array of keys */
     srand(42);
@@ -62,15 +65,19 @@ int main(int argc, char** argv)
         keys[i] = i;
     linear_shuffle(keys, num_keys);
 
-    /* sort keys using insert() / min() / successor() operations */
-    start = clock();
-    sort_veb_succ(keys, num_keys, sorted_keys);
-    end = clock();
-    for (i = 0; i < num_keys-1; i++)
-        assert(sorted_keys[i] < sorted_keys[i+1]);
+    for (t = 0; t < test_runs; t++)
+    {
+        /* sort keys using insert() / min() / successor() operations */
+        start = clock();
+        sort_veb_succ(keys, num_keys, sorted_keys);
+        end = clock();
+        elapsed += (double)(end - start) / CLOCKS_PER_SEC;
+        for (i = 0; i < num_keys-1; i++)
+            assert(sorted_keys[i] < sorted_keys[i+1]);
+    }
 
-    elapsed = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Sorting took %f milliseconds", elapsed / 1000);
+    elapsed /= test_runs;
+    printf("Sorting took %lf milliseconds", elapsed / 1000);
 
     /* sort keys using insert() / max() / predecessor() operations */
     // sort_veb_pred(keys, num_keys, sorted_keys);
@@ -78,5 +85,6 @@ int main(int argc, char** argv)
     //     assert(sorted_keys[i] < sorted_keys[i+1]);
     // TODO: enable this test when the predecessor operation is ready for use
 
+    free(keys); free(sorted_keys);
     return 0;
 }
