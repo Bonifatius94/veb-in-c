d4ab976076bf040caf775c679d9356488a0b272f
Marco Tröster <marco@troester-gmbh.de>
2022-05-02 18:14:40 +0200

    remove debug build (takes very long)

--- build.sh
+++ build.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 set -e
 
-# ===============================
-#     D E B U G   B U I L D
-# ===============================
-
-# make sure there's an empty build folder
-if [ -d "./build" ]; then rm -rf build; fi
-mkdir build
-
-# run the build and tests (debug mode)
-pushd build
-    cmake .. && cmake --build .
-    CTEST_OUTPUT_ON_FAILURE=1 ctest
-popd
-
-# ===============================
-#    R E L E A S E   B U I L D
-# ===============================
-
 # make sure there's an empty build folder
 if [ -d "./build" ]; then rm -rf build; fi
 mkdir build
